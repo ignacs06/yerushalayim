f62a6bee0cf000e6e789cdd4ef0cc561ed87d1ee
samumuni-cmd <samumuni@ucm.es>
2026-02-12 22:46:29 +0100

    Campos magnéticos a mi manera

--- Fórmulas Campos Magnéticos.py
+++ Fórmulas Campos Magnéticos.py
@@ -1,14 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  5 23:46:01 2026
+# =============================================================================
+# Sesión 3: Campos magnéticos
+# =============================================================================
 
-@author: Nacho
-"""
-L=162e-3
+#%%
+
+# =============================================================================
+# Librerías y funciones comunes:
+# =============================================================================
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from scipy.stats import t
+plt.close('all')
+
 def min_cuadrados(x,y,t):
 # =============================================================================
 #   n es el número de medidas experimentales
@@ -62,164 +67,207 @@ def min_cuadrados(x,y,t):
     delt_c=t*np.sqrt(s_c_2)
     return m,c,delt_m,delt_c
 
-ts1=t.ppf(1-0.05/2,3)
-n1=300/(162e-3)
+# =============================================================================
+# 1) Campo magnético de la bobina en función de la corriente:
+# =============================================================================
+
+L=162e-3
+d=26e-3
+N=300
+I_teo=np.linspace(0.2,1.0,10000)
+I=np.array([0.2,0.4,0.6,0.8,1.0])
+B_exp=np.array([])
 mu_0=np.pi*4e-7
-i=np.array([0.2,0.4,0.6,0.8,1.0])
-bt=mu_0*i*n1
-error_a=0.01
-error_b=1e-5
-error_t=2.3e-5
-bexp=np.array([0.00049,0.00095,0.00143,0.00191,0.00232])
-m1t,c1t,dm1,dc1=min_cuadrados(i,bt,ts1)
-m1e,c1e,dme,dce=min_cuadrados(i,bexp,ts1)
+B_teo=mu_0*I_teo*N/L
+delt_I=0
+delt_B_exp=0
+ts1=t.ppf(1-0.05/2,3)
+
+m1e,c1e,delt_me,delt_ce=min_cuadrados(I,B_exp,ts1)
+
 plt.figure()
+plt.tick_params(axis='both',labelsize=20)  
+plt.plot(I_teo,B_teo,label='$B_{teórico}$',color='blue')
+plt.plot(I_teo,m1e*I_teo+c1e,'-',color='red')
+plt.errorbar(I,B_exp,yerr=delt_B_exp,xerr=delt_I,fmt='o',color='red',ecolor='red',capsize=5)
+plt.scatter(I,B_exp,c='red',marker='o',label='$B_{experimental}$')
+plt.xlabel(r'$I~(A)$',fontsize=25)
+plt.ylabel(r'$B~(T)$',fontsize=25)
 plt.grid('True')
-plt.plot(i,bt,'o',ms=3,label='B teórico',color='blue')
-plt.plot(i,bexp,'o',ms=3,label='B experimental',color='red')
-plt.plot(i,c1t+i*m1t,linestyle='--',label='Regresión B teórico')
-plt.plot(i,c1e+i*m1e,linestyle='--',label='Regresión B experimental',color='pink')
-plt.errorbar(i, bexp, yerr=error_b, fmt='o', xerr=error_a, ms=1, color='red',ecolor='black')
-plt.errorbar(i, bt, yerr=error_t, fmt='o', xerr=error_a, ms=1, color='red',ecolor='black',label='Barras de error')
-plt.xlabel(r'$I_0$ (A)', fontsize=12)
-plt.ylabel(r'$B$ (T)', fontsize=12)
-plt.legend()
+plt.legend(fontsize=20)
+plt.gca().set_facecolor('aliceblue')
 plt.show()
 
+print('Ejercicio 1')
+print('La pendiente de la regresión es: ',m1e,'±',delt_me)
+print('La pendiente de la regresión teórica es: ',mu_0*N/L)
 
+# =============================================================================
+# 2) Campo magnético de la bobina en función del número de espiras:
+# =============================================================================
 
-#2 VARIACION NUMERO ESPIRAS
-
+N_teo=np.linspace(75,300,10000)
+N_2=np.array([75,150,300])
+B_teo2=mu_0*1*N_teo/(2*np.sqrt((L/2)**2+(d/2)**2))
+B_exp2=np.array([])
 ts2=t.ppf(1-0.05/2,1)
-nesp=np.array([75,150,300])
-bst=mu_0*1*nesp/(162e-3)
-bse=np.array([0.00077,0.00122,0.00232])
-errorbst=np.array([5.7e-6,1.1e-5,2.3e-5])
-mbst,cbst,dmbst,dcbst=min_cuadrados(nesp,bst,ts2)
-mbse,cbse,dmbse,dcbse=min_cuadrados(nesp, bse, ts2)
+
+m2e,c2e,delt_m2e,delt_c2e=min_cuadrados(N_2,B_exp2,ts2)
+
 plt.figure()
+plt.tick_params(axis='both',labelsize=20)  
+plt.plot(N_teo,B_teo2,label='$B_{teórico}$',color='blue')
+plt.plot(N_teo,m2e*N_teo+c2e,'-',color='red')
+plt.errorbar(N_2,B_exp2,yerr=delt_B_exp,xerr=0,fmt='o',color='red',ecolor='red',capsize=5)
+plt.scatter(N_2,B_exp2,c='red',marker='o',label='$B_{experimental}$')
+plt.xlabel(r'$N$',fontsize=25)
+plt.ylabel(r'$B~(T)$',fontsize=25)
 plt.grid('True')
-plt.plot(nesp,bst,'o',ms=3,color='blue',label='B teórico')
-plt.plot(nesp,bse,'o',ms=3,color='red',label='B experimental')
-plt.plot(nesp,cbst+nesp*mbst,linestyle='--',color='skyblue',label='Regresión teórica')
-plt.plot(nesp,cbse+nesp*mbse,linestyle='--',color='red',label='Regresión experimental')
-plt.errorbar(nesp, bst, yerr=errorbst, fmt='o', ms=1, color='red',ecolor='black',label='Barras de error')
-plt.xlabel(r'$N_{espiras}$ ', fontsize=12)
-plt.ylabel(r'$B$ (T)', fontsize=12)
-plt.legend()
+plt.legend(fontsize=20)
+plt.gca().set_facecolor('aliceblue')
 plt.show()
 
+print('Ejercicio 2')
+print('La pendiente de la regresión es: ',m2e,'±',delt_m2e)
+print('La pendiente de la regresión teórica es: ',mu_0*1/(2*np.sqrt((L/2)**2+(d/2)**2)))
 
-#3 15 POSIÇAOES CARMEN ES EL ARRAY DE DISTANCIAS RESPECTO DEL CENTRO
-bquince=np.array([19e-5,14.7e-4,21.4e-4,22.3e-4,22.8e-4,23e-4,23.1e-4,23.2e-4,23.3e-4,23.1e-4,23e-4,22.7e-4,22e-4,19e-4,2.3e-4])
-carmen=np.array([-0.097,-0.075,-0.06,-0.05,-0.04,-0.03,-0.02,0,0.02,0.03,0.04,0.05,0.06,0.075,0.097])
-carmenlinspace=np.linspace(-0.097,0.097,10000)
-R=13e-3
-def B3(x):
-    a=x+L/2
-    b=x-L/2
-    return (a/np.sqrt(R**2+a**2)-b/np.sqrt(R**2+b**2))*mu_0*300/(2*L)
-#Samu no te fíes mucho (NADA) de la fórmula de aquí abajo (dB3)
-def dB3(x):
-    return np.sqrt((B3(carmen)**2)*(0.01**2+(0.0001/0.162)**2+0.0022))
+# =============================================================================
+# 3) Campo magnético de la bobina a lo largo de su eje:
+# =============================================================================
+
+B_exp3=np.array([])
+x=np.array([])
+delt_x=0
+x_teo=np.linspace(x[0],x[-1],10000)
+a=x_teo+L/2
+b=x_teo-L/2
+B_teo3=mu_0*1*N/(2*L)*(a/(np.sqrt((d/2)**2+a**2))-b/(np.sqrt((d/2)**2+b**2)))
+
 plt.figure()
-plt.grid(True)
-plt.plot(carmen,bquince,'o',ms=3,label='Datos experimentales')
-plt.errorbar(carmen, bquince, yerr=dB3(carmen), fmt='o', ms=1, color='red',ecolor='black',label='Barras de error')
-plt.plot(carmenlinspace,B3(carmenlinspace),linestyle='--',color='skyblue',label='Datos teóricos')
-plt.ylabel(r'$B$ (T)', fontsize=12)
-plt.xlabel(r'$Distancia$ (m))', fontsize=12)
-plt.legend()
+plt.tick_params(axis='both',labelsize=20)  
+plt.plot(x_teo,B_teo3,label='$B_{teórico}$',color='blue')
+plt.errorbar(x,B_exp3,yerr=delt_B_exp,xerr=delt_x,fmt='o',color='red',ecolor='red',capsize=5)
+plt.scatter(x,B_exp3,c='red',marker='o',label='$B_{experimental}$')
+plt.xlabel(r'$x~(m)$',fontsize=25)
+plt.ylabel(r'$B~(T)$',fontsize=25)
+plt.grid('True')
+plt.legend(fontsize=20)
+plt.gca().set_facecolor('aliceblue')
 plt.show()
 
+# =============================================================================
+# 4) Determinación de la fem inducida en función del campo magnético:
+# =============================================================================
 
+def Ohm(I,delt_V,R=10):
+    V=I*R
+    delt_I=np.sqrt((delt_V/R)**2)
+    return V,delt_I    
 
-def ohm(V):
-    R=0.3
-    I=V/R
-    return I
+I_R=np.linspace(10e-3,30e-3,5)
+delt_V_R=0
+V_R,delt_IR=Ohm(I_R,delt_V_R)
+fem_exp=np.array([])
+delt_fem_exp=0
+f_4=10e3
+delt_f4=0
+w_4=2*np.pi*f_4
+delt_w4=2*np.pi*delt_f4
+N_sec=300
+A=np.pi*(0.5*41e-3)**2
+N_prima=0
+l_prima=0
+delt_l=0
 
-#Ley de Faraday
+def fem_teo(N,A,N_prima,w,I,l,delt_I,delt_l,delt_w):
+    fem_teo=mu_0*N*A*N_prima*w*I/l
+    delt_fem_teo=np.sqrt((mu_0*N*A*N_prima*w*delt_I/l)**2+(mu_0*N*A*N_prima*w*I*delt_l/l**2)**2+(mu_0*N*A*N_prima*delt_w*I/l)**2)
+    return fem_teo,delt_fem_teo
 
-'''Defino la función que da el voltaje inducido en la bobina
-secundaria a partir de la ecuación (7) del guión AYUDA'''
-def xi(I,f,N):
-    df=10
-    dI=1e-3
-    dl=1e-3 
-    dd=1e-3
-    d=0.041
-    l=0.75
-    w=2*np.pi*f
-    Np=485
-    A=np.pi*(d/2)**2
-    xi=mu_0	*N*A*Np*w*(I/l)
-    ixi=mu_0*N*Np*np.sqrt((2*A*np.pi*I*df/l)**2+(2*np.pi*A*f*dI/l)**2+
-                          (A*I*f*dl/(l**2))**2+(np.pi**2*f*I*d*dd/l)**2)
-    return xi,ixi
+I_teo4=np.linspace(10e-3,30e-3,10000)
+fem_teo4,delt_fem_teo4=fem_teo(N_sec,A,N_prima,w_4,I_teo4,l_prima,delt_IR,delt_l)
 
-'Samu de esta función de mierda (xi) sí te puedes fiar porque llevo veinte minutos'
-'para calcular la incertidumbre de 5 putos datos' '''SÍ SAMUEL ESTÁ TODO EN SISTEMA'''
-'''INTERNACIONAL'''
+m3e,c3e,delt_m3e,delt_c3e=min_cuadrados(I_R,fem_exp,ts1)
 
-'''WE ARE CHARLIE KIRK'''    
-
-
-
-I0=np.array([0.0146,0.0180,0.02168,0.02595,0.02988])
-I0l=np.linspace(0.0146,0.02988,10000)
-Aie=np.array([0.2830,0.3618,0.4226,0.5076,0.5572])
-wearecharliekirk=min_cuadrados(I0, Aie, ts1)
 plt.figure()
-plt.grid(True)
-plt.xlabel(r'$I_0$ (A)', fontsize=12)
-plt.ylabel(r'$Amplitud_{ind}$ (V)', fontsize=12)
-plt.plot(I0l,xi(I0l,1e4,300)[0],color='blue')
-plt.plot(I0,xi(I0,1e4,300)[0],ms=3,color='blue',label='Valores teóricos')
-plt.plot(I0,Aie,'o',ms=3,color='red',label='Valores experimentales')
-plt.plot(I0l,wearecharliekirk[1]+wearecharliekirk[0]*I0l,linestyle='--')
-plt.errorbar(I0,xi(I0,1e4,300)[0],xerr=1e-3,yerr=xi(I0,1e4,300)[1],fmt='o',ms=1,color='blue',ecolor='black')
-plt.errorbar(I0, Aie, yerr=1e-4, fmt='o', ms=1, color='red',ecolor='black',label='Barras de error')
-plt.legend()
-
-#Apartado 5
-I5=0.03
-f5=np.array([3000,5010,7010,10000,12020])
-f5l=np.linspace(3000,12020,1000)
-A5e=np.array([0.2075,0.2621,0.4588,0.5076,0.5826])
-jeffreyepstein=min_cuadrados(f5,A5e,ts1)
-plt.figure()
-plt.grid(True)
-plt.xlabel(r'$Frecuencia$ (Hz)', fontsize=12)
-plt.ylabel(r'$Amplitud_{ind}$ (V)', fontsize=12)
-plt.plot(f5l,xi(0.03,f5l,300)[0],color='blue')
-plt.plot(f5,xi(0.03,f5,300)[0],ms=3,color='blue',label='Valores teóricos')
-plt.plot(f5,A5e,'o',ms=3,color='red',label='Valores experimentales')
-plt.plot(f5l,jeffreyepstein[1]+jeffreyepstein[0]*f5l,linestyle='--')
-plt.errorbar(f5,xi(0.03,f5,300)[0],xerr=10,yerr=xi(0.03,f5,300)[1],fmt='o',ms=1,color='blue',ecolor='black')
-plt.errorbar(f5, A5e, yerr=1e-4, fmt='o', ms=1, color='red',ecolor='black')
-plt.errorbar(f5,A5e,xerr=10,yerr=1e-4,fmt='o',ms=1,color='blue',ecolor='purple',label='Barras de error')
-plt.legend()
-
-#Apartado 6
-f6=1e4
-N6l=np.linspace(100,300,200)
-N6=np.array([100,200,300])
-A6e=np.array([0.2090,0.4079,0.7178])
-netanyahu=min_cuadrados(N6,A6e,ts2)
-plt.figure()
-plt.grid(True)
-plt.xlabel(r'Nº de espiras', fontsize=12)
-plt.ylabel(r'$Amplitud_{ind}$ (V)', fontsize=12)
-plt.plot(N6l,xi(I5,f6,N6l)[0],color='blue')
-plt.plot(N6,xi(I5,f6,N6)[0],ms=3,color='blue',label='Valores teóricos')
-plt.plot(N6,A6e,'o',ms=3,color='red',label='Valores experimentales')
-plt.plot(N6l,netanyahu[1]+netanyahu[0]*N6l,linestyle='--',label='Regresión experimental')
-plt.errorbar(N6,xi(0.03,f6,N6)[0],xerr=10,yerr=xi(0.03,f6,N6)[1],fmt='o',ms=1,color='blue',ecolor='black')
-plt.errorbar(N6, A6e, yerr=1e-4, fmt='o', ms=1, color='red',ecolor='black')
-plt.errorbar(N6,A6e,xerr=1,yerr=1e-4,fmt='o',ms=1,color='blue',ecolor='purple',label='Barras de error')
-plt.legend()
+plt.tick_params(axis='both',labelsize=20)  
+plt.plot(I_teo4,fem_teo4,label='$\epsilon_{teórico}$',color='blue')
+plt.plot(I_teo4,m3e*I_teo4+c3e,'-',color='red')
+plt.errorbar(I_R,fem_exp,yerr=delt_fem_exp,xerr=delt_IR,fmt='o',color='red',ecolor='red',capsize=5)
+plt.scatter(I_R,fem_exp,c='red',marker='o',label='$\epsilon_{experimental}$')
+plt.xlabel(r'$I~(A)$',fontsize=25)
+plt.ylabel(r'$\epsilon~(V)$',fontsize=25)
+plt.grid('True')
+plt.legend(fontsize=20)
+plt.gca().set_facecolor('aliceblue')
 plt.show()
 
+print('Ejercicio 4')
+print('La pendiente de la regresión es: ',m3e,'±',delt_m3e) 
+print('La pendiente de la regresión teórica es: ',mu_0*N_sec*A*N_prima*w_4/l_prima)
 
+# =============================================================================
+# 5) Determinación de la fem inducida en función de la frecuencia:
+# =============================================================================
 
+I_5=30e-3
+V_5,delt_I_5=Ohm(I_5,delt_V_R)
+f_5=np.linspace(1000,12000,5)
+delt_f_5=0
+w_5=2*np.pi*f_5
+delt_w_5=2*np.pi*delt_f_5
+fem_exp5=np.array([])
+delt_fem_exp5=0
+f_teo=np.linspace(1000,12000,10000)
+w_teo=2*np.pi*f_teo
+fem_teo5,delt_fem_teo5=fem_teo(N_sec,A,N_prima,w_teo,I_5,l_prima,delt_I_5,delt_l,delt_w_5)
+m4e,c4e,delt_m4e,delt_c4e=min_cuadrados(f_5,fem_exp5,ts1)
+
+plt.figure()
+plt.tick_params(axis='both',labelsize=20)  
+plt.plot(f_teo,fem_teo5,label='$\epsilon_{teórico}$',color='blue')
+plt.plot(f_teo,m4e*f_teo+c4e,'-',color='red')
+plt.errorbar(f_5,fem_exp5,yerr=delt_fem_exp5,xerr=delt_f_5,fmt='o',color='red',ecolor='red',capsize=5)
+plt.scatter(f_5,fem_exp5,c='red',marker='o',label='$\epsilon_{experimental}$')
+plt.xlabel(r'$f~(Hz)$',fontsize=25)
+plt.ylabel(r'$\epsilon~(V)$',fontsize=25)
+plt.grid('True')
+plt.legend(fontsize=20)
+plt.gca().set_facecolor('aliceblue')
+plt.show()
+
+print('Ejercicio 5')
+print('La pendiente de la regresión es: ',m4e,'±',delt_m4e)
+print('La pendiente de la regresión teórica es: ',mu_0*N_sec*A*N_prima*I_5*l_prima**(-1)*2*np.pi)
+
+# =============================================================================
+# 5) Determinación de la fem inducida en función de la frecuencia:
+# =============================================================================
+
+f_6=1e4
+delt_f6=0
+w_6=2*np.pi*f_6
+delt_w6=2*np.pi*delt_f6
+N_6=np.array([100,200,300])
+fem_exp6=np.array([])
+delt_fem_exp6=0
+N_teo=np.linspace(100,300,10000)
+fem_teo6,delt_fem_teo6=fem_teo(N_teo,A,N_prima,w_6,I_5,l_prima,delt_I_5,delt_l,delt_w6)
+m5e,c5e,delt_m5e,delt_c5e=min_cuadrados(N_6,fem_exp6,ts2)
+
+plt.figure()
+plt.tick_params(axis='both',labelsize=20)
+plt.plot(N_teo,fem_teo6,label='$\epsilon_{teórico}$',color='blue')
+plt.plot(N_teo,m5e*N_teo+c5e,'-',color='red')
+plt.errorbar(N_6,fem_exp6,yerr=delt_fem_exp6,xerr=0,fmt='o',color='red',ecolor='red',capsize=5)
+plt.scatter(N_6,fem_exp6,c='red',marker='o',label='$\epsilon_{experimental}$')
+plt.xlabel(r'$N$',fontsize=25)
+plt.ylabel(r'$\epsilon~(V)$',fontsize=25)
+plt.grid('True')
+plt.legend(fontsize=20)
+plt.gca().set_facecolor('aliceblue')
+plt.show()
+
+print('Ejercicio 6')
+print('La pendiente de la regresión es: ',m5e,'±',delt_m5e)
+print('La pendiente de la regresión teórica es: ',mu_0*A*N_prima*w_6*I_5/l_prima)
